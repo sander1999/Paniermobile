859d01df1dc7fc79856d98783b28145bad398be3
Terogene Claudio <cterogene@gmail.com>
2022-05-27 10:25:16 -0500

    added the modify product page and created the page for better image and user.

---
+++ app/src/main/java/com/example/paniermobil/Models/modify_Products.java
@@ -0,0 +1,16 @@
+package com.example.paniermobil.Models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.paniermobil.R;
+
+public class modify_Products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_products);
+    }
+}
